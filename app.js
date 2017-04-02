1f25316a2bfa4a1f98585829cedc1d48e3760992
Ajinkya D <dube.ajinkya452@gmail.com>
2017-04-02 12:31:02 +0530

    new changes

--- app.js
+++ app.js
@@ -11,10 +11,36 @@ const PORT=8080;
 app.get('/', function (req, res1) {
    
         res1.end("Hello Welcome ");
+        //res1.sendFile('index.html');
     
 })
 
-FB.options({accessToken: 'EAACEdEose0cBACZBH4HVCpIlfAYVYtcXdfqU6mNL9HqTQKVzZB0PLJC6cWGmsQZArlxAxnyEqbHgi8oCshp9tInUlYYVwu48jX9NNQrHLu9w9er8khuRrtrjFNKeVbX76OsEPJ0Ru8ZCaZBM8T9YSYeYgYXrsKAlPZBllXNKFcxwZDZD'});
+
+
+FB.api('oauth/access_token', {
+    client_id: '866100573474781',
+    client_secret: '3326fb75cb54a353ae91194bef7e15b7',
+    grant_type: 'client_credentials'
+}, function (res) {
+    if(!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+    }
+    console.log('token---->',res.access_token);
+ 
+    var accessToken = res.access_token;
+    FB.setAccessToken(accessToken);
+    var accessToken = FB.getAccessToken();
+
+
+FB.api('me', { fields: ['id', 'name'], access_token: accessToken }, function (res) {
+    console.log(res);
+});
+    //FB.options({accessToken: 'EAACEdEose0cBACZBH4HVCpIlfAYVYtcXdfqU6mNL9HqTQKVzZB0PLJC6cWGmsQZArlxAxnyEqbHgi8oCshp9tInUlYYVwu48jX9NNQrHLu9w9er8khuRrtrjFNKeVbX76OsEPJ0Ru8ZCaZBM8T9YSYeYgYXrsKAlPZBllXNKFcxwZDZD'});
+
+});
+
+
 
 app.get('/mylikes', function (req, res1) {
    FB.api('me?fields=likes.limit(10){name}', function (res) {
